223999c84f97c30fb555209025f33b067b2b8966
Sowmayjain <champ.sowmay@gmail.com>
2019-03-04 02:44:22 +0530

    proxy test with events.

--- contracts/v2/ProxyTest.sol
+++ contracts/v2/ProxyTest.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.4.23;
 
 contract ProxyTest {
 
+    event ETHSent(uint amt);
+
     function sendETH() public payable {
         address(msg.sender).transfer(msg.value);
+        emit ETHSent(msg.value);
     }
 
 }
